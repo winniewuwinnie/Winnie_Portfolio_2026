ef6ac3c7e3040d5a3ec5ba2f2ffed5c079a8c08a
Winnie <107299196+winniewuwinnie@users.noreply.github.com>
2026-03-30 01:07:34 +1300

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -250,7 +250,7 @@ DateFormat.fn.init.prototype = DateFormat.fn;
 
 
 // ========================
-// class slick
+// projects slick
 // ========================
 $(document).on('ready', function () {
   $(".slick_L").slick({
@@ -351,24 +351,8 @@ $(document).on('ready', function () {
     responsive: [{
         breakpoint: 1024,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
+          slidesToShow: 3,
+          slidesToScroll:3,
           infinite: true,
         }
       }
